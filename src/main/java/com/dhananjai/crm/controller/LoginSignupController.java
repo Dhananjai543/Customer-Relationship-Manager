cb31f1a5c7b6af1cc62ff66ebfc3c89bf49b704a
Dhananjai <dhananjaisaini9@gmail.com>
2024-07-10 19:46:01 +0530

    Added css styles

--- src/main/java/com/dhananjai/crm/controller/LoginSignupController.java
+++ src/main/java/com/dhananjai/crm/controller/LoginSignupController.java
@@ -86,7 +86,15 @@ public class LoginSignupController {
 	}
 	
 	@GetMapping("/showSignUpForm")
-	public String showSignupForm(Model theModel) {
+	public String showSignupForm(Model theModel, HttpServletResponse response) {
+		Cookie jwtCookie = new Cookie("jwtToken", null);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setSecure(true);
+        jwtCookie.setPath("/"); // Ensure the path matches where the cookie was set
+        jwtCookie.setMaxAge(0); // Set max age to 0 to delete the cookie
+
+        // Add the expired cookie to the response
+        response.addCookie(jwtCookie);
 		User user = new User();
 		theModel.addAttribute("user", user);
 		return "sign-up-form";
@@ -94,7 +102,7 @@ public class LoginSignupController {
 	
 	@PostMapping("/processSignUpForm")
 	public String processSignUpForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult,
-			Model model) {
+			Model model, HttpServletResponse response) {
 
 		// validation errors from user Entity
 		if (bindingResult.hasErrors()) {
@@ -128,9 +136,38 @@ public class LoginSignupController {
 				return "sign-up-form";
 			}
 			
-			AuthenticationResponse response = authenticationService.register(user);
+			AuthenticationResponse authResponse = authenticationService.register(user);
+			
+			// -----
+			
+			AuthenticationRequest request = new AuthenticationRequest();
+			request.setUsername(user.getUsername());
+			request.setPassword(user.getPassword());
+			System.out.println(request.toString());
+			ResponseEntity<AuthenticationResponse> currResponse;
+			
+			try {
+				currResponse = ResponseEntity.ok(authenticationService.login(request, response));
+				System.out.println(currResponse.getBody().gettoken());
+				
+				Cookie jwtCookie = new Cookie("jwtToken", currResponse.getBody().gettoken());
+		        jwtCookie.setHttpOnly(true); // To prevent JavaScript access to the cookie
+		        jwtCookie.setSecure(true); // To ensure the cookie is sent over HTTPS
+		        jwtCookie.setPath("/"); // The path to which the cookie applies
+		        jwtCookie.setMaxAge(60 * 60 * 24); // Cookie valid for 1 day
+
+		        // Add the cookie to the response
+		        response.addCookie(jwtCookie);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				model.addAttribute("error","Sorry! Error occured while signing up.");
+				return "login-form";
+			}
+			
+			// -----
+			
 			model.addAttribute("accountCreated", true);
-			return "success ";
+			return "redirect:/customer/list";
 		} catch (Exception e) {
 			System.out.println("Exception: " + e.getMessage());
 			model.addAttribute("error", "There already exists an account with this email");
